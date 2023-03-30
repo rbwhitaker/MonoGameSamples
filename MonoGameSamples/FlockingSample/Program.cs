4676e77c1314f650c57a4b23c7ec4f1a74f04eb3
rbwhitakerasi <rb.whitaker@asirobots.com>
2023-03-30 15:07:21 -0600

    Ported the FlockingSample project.

---
+++ MonoGameSamples/FlockingSample/Program.cs
@@ -0,0 +1,31 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// Program.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+#endregion
+
+namespace Flocking
+{
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (FlockingSample game = new FlockingSample())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+
