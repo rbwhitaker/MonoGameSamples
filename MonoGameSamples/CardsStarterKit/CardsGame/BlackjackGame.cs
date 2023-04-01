93aeb235be32ebbb3becb24709ec92b7522171eb
RB Whitaker <rbwhitaker@outlook.com>
2023-03-31 19:51:14 -0600

    Ported the Cards (Blackjack) starter kit.

---
+++ MonoGameSamples/CardsStarterKit/CardsGame/BlackjackGame.cs
@@ -0,0 +1,87 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// PauseScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using CardsFramework;
+using System.Globalization;
+using GameStateManagement;
+#endregion
+
+namespace Blackjack
+{
+    /// <summary>
+    /// This is the main game type.
+    /// </summary>
+    public class BlackjackGame : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        ScreenManager screenManager;
+
+        public static float HeightScale = 1.0f;
+        public static float WidthScale = 1.0f;
+
+        /// <summary>
+        /// Initializes a new instance of the game.
+        /// </summary>
+        public BlackjackGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+
+            screenManager = new ScreenManager(this);
+
+            screenManager.AddScreen(new BackgroundScreen(), null);
+            screenManager.AddScreen(new MainMenuScreen(), null);
+
+            Components.Add(screenManager);
+
+            IsMouseVisible = true;
+
+            // Initialize sound system
+            AudioManager.Initialize(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            graphics.PreferredBackBufferHeight = 480;
+            graphics.PreferredBackBufferWidth = 800; 
+      
+            graphics.ApplyChanges();
+
+            Rectangle bounds = graphics.GraphicsDevice.Viewport.TitleSafeArea;
+            HeightScale = bounds.Height / 480f;
+            WidthScale = bounds.Width / 800f;
+
+
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            AudioManager.LoadSounds();
+
+            base.LoadContent();
+        }
+    }
+}
---
+++ MonoGameSamples/CardsStarterKit/CardsGame/Program.cs
@@ -0,0 +1,4 @@
+using (Blackjack.BlackjackGame game = new Blackjack.BlackjackGame())
+{
+    game.Run();
+}
